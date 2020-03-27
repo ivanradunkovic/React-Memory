dec303cff943732bb581a31c17350f86c4bfa7f4
Ivan <ivan1501@gmail.com>
2020-03-27 13:14:30 +0100

    Declared game reducer, creted game index and game utils wiht hook which will generate game field.

---
+++ src/game/game.reducer.js
@@ -0,0 +1,38 @@
+import { merge } from 'ramda';
+import levels from '../config/levels';
+
+export const NEW_LEVEL = 'level/new';
+export const HIDDEN_CELL_HIDE = 'hidden/hide';
+export const HIDDEN_CELL_SHOW = 'hidden/show';
+export const FIELD_HIDE = 'field/hide';
+export const FIELD_SHOW = 'field/show';
+export const RESET_LEVEL = 'level/reset';
+
+const START_LEVEL = 0;
+
+export const initialState = {
+    level: START_LEVEL,
+    showHidden: true,
+    showField: false,
+    levelConfig: levels[START_LEVEL],
+}
+
+export function GameReducer(state, action) {
+    
+    switch(action.type) {
+        case NEW_LEVEL:
+            return merge(state, { level: action.level, levelConfig: levels[action.level] });
+        case HIDDEN_CELL_SHOW:
+            return merge(state, { showHidden: true });
+        case HIDDEN_CELL_HIDE:
+            return merge(state, { showHidden: false });
+        case FIELD_HIDE:
+            return merge(state, { showField: false });
+        case FIELD_SHOW:
+            return merge(state, { showField: true });
+        case RESET_LEVEL:
+            return merge(initialState, { levelConfig: { ...levels[START_LEVEL] } });
+        default:
+            return state;
+    }
+}
---
+++ src/game/index.jsx
@@ -0,0 +1,61 @@
+import React, { memo, useReducer, useMemo, useEffect } from 'react';
+import { Field } from './components/GameField';
+import { GameFieldView, GameView, SwitchView } from './components/Styled';
+import { GameReducer, initialState, NEW_LEVEL, FIELD_HIDE, FIELD_SHOW, RESET_LEVEL } from './game.reducer';
+import { generateGameField } from './game.utils';
+import Switch from 'rc-switch';
+import 'rc-switch/assets/index.css';
+
+function Game ({ toggleTheme }) {
+
+    const [{ level, showHidden, showField, levelConfig }, dispatch] = useReducer(
+        GameReducer, initialState
+    );
+
+    // const levelConfig = levels[level];
+    const { cellCount, memoryCount } = levelConfig;
+    const { field, hiddenCells } = useMemo(
+        () => generateGameField(cellCount, memoryCount),
+        [levelConfig]
+    );
+
+    console.log('field', field);
+    
+    useEffect(
+        () => setTimeout(dispatch, 500, { type: FIELD_SHOW }),
+        [levelConfig],
+    );
+
+    function updateLevel(shouldReset) {
+        
+        dispatch({ type: FIELD_HIDE });
+        setTimeout(dispatch, 500, { type: shouldReset ? RESET_LEVEL : NEW_LEVEL, level: level + 1 });
+    }
+
+    return (
+        <GameView>
+            <GameFieldView {...levelConfig}>
+                <SwitchView>
+                    <div>Level: {level}</div>
+                    <div>
+                        Theme mode: <Switch onClick={toggleTheme} />
+                    </div>
+                </SwitchView>
+                <Field
+                    {...levelConfig}
+                    levelConfig={levelConfig}
+                    visible={showField}
+                    key={field}
+                    level={level}
+                    field={field}
+                    hiddenCells={hiddenCells}
+                    dispatch={dispatch}
+                    showHidden={showHidden}
+                    updateLevel={updateLevel}
+                />
+            </GameFieldView>
+        </GameView>
+    );
+}
+
+export default memo(Game);
